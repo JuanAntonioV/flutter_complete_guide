1eab01e3b51e6ad8d5daf366783a042e370a753d
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-01-10 23:10:21 +0700

    Elevated Button

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('My First App'),
-        ),
-        body: Text('This is my default text!'),
-      ),
+          appBar: AppBar(
+            title: const Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              ElevatedButton(onPressed: () {}, child: Text('Answer 1')),
+              ElevatedButton(onPressed: () {}, child: Text('Answer 2')),
+              ElevatedButton(onPressed: () {}, child: Text('Answer 3'))
+            ],
+          )),
     );
   }
 }
