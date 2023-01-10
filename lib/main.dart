0f866a0ca5036a20c8ec712d0c059632b93b2973
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-01-10 23:06:09 +0700

    Scaffold appBar & body

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My First App'),
+        ),
+        body: Text('This is my default text!'),
+      ),
     );
   }
 }
